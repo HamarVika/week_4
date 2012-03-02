ad771321245d4ee0a8235fb63c4b18377974419a
Vika <Vika_lviv@mail.ru>
2012-03-02 10:38:17 +0200

    Update dll/useticlib/useticlib/useticlib.h

--- dll/useticlib/useticlib/useticlib.h
+++ dll/useticlib/useticlib/useticlib.h
@@ -5,9 +5,8 @@
 using namespace std;
 typedef unsigned short int ui;
 
-typedef  int (*askNumber)(string question, ui high, ui low);
+
 typedef  char (*winner)(const vector<char>* const board);
-typedef  bool (*isLegal)(int move, const vector<char>* const board);
 typedef int (*computerMove)(vector<char> board, char computer);
 typedef	int (*humanMove)(const vector<char>* const board);
 
